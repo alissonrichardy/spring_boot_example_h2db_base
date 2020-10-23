e32dea7705e8dbffdaa25dd58b8679ff4ca33875
Alisson Zubcov <alissonrichardy@hotmail.com>
2020-10-22 23:08:50 -0300

    primeiro commit

---
+++ src/main/java/br/com/zubcov/example/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.zubcov.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
